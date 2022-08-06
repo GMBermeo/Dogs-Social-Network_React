173b8f1c9cdf0b6740a55125a2e780aa61ca2380
Guilherme Bermêo <gm@bermeo.dev>
2022-08-06 09:26:54 -0300

    📱 0818 Mobile Menu

---
+++ src/components/common/UserHeader/UserHeaderNav.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { UserContext } from "../../../contexts/UserContext";
+import { useMedia } from "../../../lib/hooks/useMedia";
+import s from "./UserHeaderNav.module.css";
+import { ReactComponent as FeedIcon } from "../../../assets/svg/feed.svg";
+import { ReactComponent as StatsIcon } from "../../../assets/svg/stats.svg";
+import { ReactComponent as PostIcon } from "../../../assets/svg/post.svg";
+import { ReactComponent as LogoutIcon } from "../../../assets/svg/logout.svg";
+
+const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia("(max-width: 40rem)");
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
+  return (
+    <>
+      {mobile && (
+        <button
+          aria-label="Menu"
+          className={`${s.mobileButton} ${
+            mobileMenu ? s.mobileButtonActive : ""
+          }`}
+          onClick={() => {
+            setMobileMenu(!mobileMenu);
+          }}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? s.navMobile : s.nav} ${
+          mobileMenu && s.navMobileActive
+        }`}
+      >
+        <NavLink to="/myAccount" end>
+          <FeedIcon />
+          {mobile && "My Feed"}
+        </NavLink>
+        <NavLink to="stats">
+          <StatsIcon />
+          {mobile && "Stats"}
+        </NavLink>
+        <NavLink to="post">
+          <PostIcon />
+          {mobile && "Post photo"}
+        </NavLink>
+        <button onClick={userLogout}>
+          <LogoutIcon />
+          {mobile && "Logout"}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default UserHeaderNav;
--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,4 +1,4 @@
-export { default as UserHeader } from "./UserHeader/UserHeader";
-export { default as UserHeaderNav } from "./UserHeader/UserHeaderNav";
+export { default as UserHeader } from "../common/UserHeader/UserHeader";
+export { default as UserHeaderNav } from "../common/UserHeader/UserHeaderNav";
 export { default as UserPhotoPost } from "./UserPhotoPost";
 export { default as UserStats } from "./UserStats";
